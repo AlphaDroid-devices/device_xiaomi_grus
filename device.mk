e38f0bfab076242230a8d8d2aabfbe7f343f2aec
elpaablo <donopwhile1@gmail.com>
2023-11-20 10:01:51 +0000

    Revert "Move to QTI health AIDL service"
    This reverts commit e5644f480ba90a84d5e27941b19295f64c5c01ac.

--- device.mk
+++ device.mk
@@ -186,8 +186,9 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health-service.qti \
-    android.hardware.health-service.qti_recovery
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
 
 # HIDL
 PRODUCT_PACKAGES += \
