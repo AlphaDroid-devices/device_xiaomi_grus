ba95200e1d044b90e215196013347434838ccb24
Rocky7842 <eric.rocky7842@gmail.com>
2025-03-06 23:46:05 +0800

    grus: Configure product name prop override
    * Match stock.
    
    Change-Id: Ib3974a7019ec1ee38d578c97caefb3ad891f26cd

--- lineage_grus.mk
+++ lineage_grus.mk
@@ -25,4 +25,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="grus-user 11 RKQ1.200826.002 V12.5.1.0.RFBMIXM release-keys" \
-    BuildFingerprint=Xiaomi/grus/grus:11/RKQ1.200826.002/V12.5.1.0.RFBMIXM:user/release-keys
+    BuildFingerprint=Xiaomi/grus/grus:11/RKQ1.200826.002/V12.5.1.0.RFBMIXM:user/release-keys \
+    DeviceProduct=grus \
+    SystemName=grus
