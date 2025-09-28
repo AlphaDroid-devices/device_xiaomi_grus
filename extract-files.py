kyasu/android_device_xiaomi_grus

grus: Patch audio to load libaudioroute-v34.so
Change-Id: Idad20512982b8753820fffa6d956df137e462638

extract-files.py
@@ -98,6 +98,11 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/lib64/mediadrm/libwvdrmengine.so',
     ): blob_fixup()
         .add_needed('libcrypto_shim.so'),
+    (
+        'vendor/lib/hw/audio.primary.sdm710.so',
+        'vendor/lib/libaudioroute_ext.so'
+    ): blob_fixup()
+        .replace_needed('libaudioroute.so', 'libaudioroute-v34.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(

AlphaDroid-devices/device_xiaomi_grus

extract-files.py
@@ -39,6 +39,11 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('/vendor/lib/libssrec.so', '/odm/lib/libssrec.so'),
     'vendor/lib/libswregistrationalgo.so': blob_fixup()
         .replace_needed('libprotobuf-cpp-full.so', 'libprotobuf-cpp-full-3.9.1.so'),
+    (
+        'vendor/lib/hw/audio.primary.grus.so',
+        'vendor/lib/libaudioroute_ext.so'
+    ): blob_fixup()
+        .replace_needed('libaudioroute.so', 'libaudioroute-v34.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(