a0793ee36582b958fc86f14aa15b28c85b0b21ea
kyasu <kyasu.dev@gmail.com>
2025-11-29 17:40:50 +0900

    Revert "grus: Retrofit dynamic partitions"
    This reverts commit f58fa332fa74d9743f570c45b91df89fa5609c68.

--- device.mk
+++ device.mk
@@ -196,10 +196,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
 
-# Fastbootd
-PRODUCT_PACKAGES += \
-    fastbootd
-
 # Filesystem
 PRODUCT_PACKAGES += \
     e2fsck_ramdisk \
@@ -308,8 +304,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 # Partitions
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
 PRODUCT_PACKAGES += \
     vendor_bt_firmware_mountpoint \
     vendor_dsp_mountpoint \
