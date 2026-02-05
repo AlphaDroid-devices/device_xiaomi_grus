9ad329348b9cf7f7d653414ba2c0c8acc4fa47c6
Rocky7842 <eric.rocky7842@gmail.com>
2026-02-05 14:11:47 +0800

    grus: Convert soong config to bool
    Change-Id: Idb9d5ebcca9d1b4b32f7f8bb98c85bd5b06f0ae4

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@ PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Audio
-$(call soong_config_set,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
+$(call soong_config_set_bool,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
 
 # Audio configs
 PRODUCT_COPY_FILES += \
